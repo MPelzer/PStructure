b51e22931716ee9c388d09a845525aa7bd3d50b4
Matthias Pelzer <mptm@posteo.de>
2024-08-27 21:56:56 +0200

    Interface, welches die Aufgabe übernimmt die Tabellenspaltennamen auf die Parameter zu zeichnen. Basisinterface angerissen, nicht sicher, ob es tatsächlich genutzt wird.

---
+++ PStructure/Mapper/IMapperPdoQuery.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using Dapper;
+
+namespace PStructure
+{
+    
+    public interface IMapperPdoQuery<T>
+    {
+        void MapTableColumnsToPdo(T entity, IDataReader reader);
+        void MapPdoToTable(T item, DynamicParameters parameters);
+        void MapPrimaryKeysToParameters(T item, DynamicParameters parameters);
+    }
+
+}
---
+++ PStructure/root/IPBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Text.Json;
+
+public interface IPBase
+{
+    string ToStringForTest();
+
+    bool Validate();
+
+    string ToJson();
+
+    T FromJson<T>(string json);
+
+    IPBase Clone();
+}
