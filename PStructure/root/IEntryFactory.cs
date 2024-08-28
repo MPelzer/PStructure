375d3e25da86b4902b251e3a2eee41e83351a045
Matthias Pelzer <mptm@posteo.de>
2024-08-28 20:18:11 +0200

    PBase Funktionen in die Factory geschoben. Wow, 2 Tage gebraucht, um die ganzen Commits nachzuziehen...

---
+++ PStructure/root/IEntryFactory.cs
@@ -0,0 +1,38 @@
+﻿namespace PStructure.Interfaces
+{
+    public interface IItemFactory <T>
+    {
+        /// <summary>
+        /// Erstellt ein Item mit Standardwerten.               
+        /// </summary>
+        /// <returns></returns>
+        T CreateDefaultEntry();
+        
+        /// <summary>
+        /// Klont das PDO
+        /// </summary>
+        /// <param name="itemToClone"></param>
+        /// <returns></returns>
+        T CloneEntry(T itemToClone);
+        
+        /// <summary>
+        /// Ein Item zu JSON
+        /// </summary>
+        /// <returns></returns>
+        string ToJson();
+
+        /// <summary>
+        /// Ein Item von JSON
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        T FromJson(string json);
+        
+        /// <summary>
+        /// Gibt das Objekt in Schreibweise KlassenName:value,Eigenschlaft1:value,Eigenschlaft2:value,... aus.
+        /// </summary>
+        /// <returns></returns>
+        string ToStringForTest();
+        
+    }
+}
